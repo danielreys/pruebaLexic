8ac7c37a7f33599f5ca866d48b6d7ec92ecd8652
danireyes5015@gmail.com <danireyes5015@gmail.com>
2021-06-17 17:36:45 +0200

    Estructura elementos

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Lexic</title>
+        <link rel="stylesheet" href="./styles.css" />
+    </head>
+    <body>
+        <div id="sec1" class="sec">
+        
+        </div>
+        <div id="sec2" class="sec">
+        
+        </div>
+        <div id="sec3" class="sec">
+        
+        </div>
+        <div class="my-footer">
+            
+        </div>
+    </body>
+</html>
