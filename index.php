f32ead69c02e413c4b9648b6d5124d1eca73b7c0
danireyes5015@gmail.com <danireyes5015@gmail.com>
2021-06-17 19:57:58 +0200

    Section2 and its styles

--- index.php
+++ index.php
@@ -11,13 +11,13 @@
         <div id="sec1" class="sec">
            <div id="sec1Title">
               <div id="text">
-                PLEASE FEEL FREE TO FILL THE FORM, WE WILL CALL YOU AS SOON AS POSSIBLE!
+                <p>PLEASE FEEL FREE TO FILL THE FORM, WE WILL CALL YOU AS SOON AS POSSIBLE! </p>
               </div>  
            </div>
            <div id="sec1Form">
                <div id="wrappForm">
                     <h2>Fill the next form</h2>
-                    <div>You agree to be contacted by us and use your contact info in the future.</div>
+                    <div> <p>You agree to be contacted by us and use your contact info in the future.</p></div>
                      <form id="formContact">
                             <input type="text" id="fname" class="inputForm" name="fname" placeholder="Full Name*"><br>
                             <input type="email" id="email" class="inputForm" name="email" placeholder="Email address*"><br>
@@ -29,7 +29,38 @@
             </div>  
         </div>
         <div id="sec2" class="sec">
-        
+            <div id="wrapperSec2">
+                <div id="sec2Info">
+                    <div id="sec2Title">LOREM IPSUM IS SIMPLY DUMMY TEXT BUT VERY NICE</div>
+                    <div id="sec2Text">
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p> 
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>  
+                    </div>
+                    <hr style="width:80px; opacity: 0.3;">
+                    <p class="bold">Lorem Ipsum Dolor: 12345678</p>
+                    <p class="bold">Period: 1st of April 2019 - 28th of June 2019</p>
+                        
+                    <div class="cards">
+                        <div class="card bold">
+                            <img src="./assets/Goal.png"/>
+                            <div id="amount1" class="amount">£0</div>
+                            <p class="amountP">Total Purchase Goal</p>
+                        </div>
+                        <div class="card bold">
+                            <img src="./assets/Sales.png"/>
+                            <div id="amount2" class="amount">£0</div>
+                            <p class="amountP">Total Purchase Goal</p>
+                        </div>
+                        <div class="card bold">
+                            <img src="./assets/GoalRemaining.png"/>
+                            <div id="amount3" class="amount">£0</div>
+                            <p class="amountP">Total Purchase Goal</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
         <div id="sec3" class="sec">
         
