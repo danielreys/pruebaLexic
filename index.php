1b250632284a646ee2fd152c58bcf84492a6bd65
danireyes5015@gmail.com <danireyes5015@gmail.com>
2021-06-17 18:58:42 +0200

    section 1 styles

--- index.php
+++ index.php
@@ -9,7 +9,24 @@
     </head>
     <body>
         <div id="sec1" class="sec">
-        
+           <div id="sec1Title">
+              <div id="text">
+                PLEASE FEEL FREE TO FILL THE FORM, WE WILL CALL YOU AS SOON AS POSSIBLE!
+              </div>  
+           </div>
+           <div id="sec1Form">
+               <div id="wrappForm">
+                    <h2>Fill the next form</h2>
+                    <div>You agree to be contacted by us and use your contact info in the future.</div>
+                     <form id="formContact">
+                            <input type="text" id="fname" class="inputForm" name="fname" placeholder="Full Name*"><br>
+                            <input type="email" id="email" class="inputForm" name="email" placeholder="Email address*"><br>
+                            <input type="text" id="company" class="inputForm" name="company" placeholder="Company*"><br>
+                            <input type="text" id="phoneNumber" class="inputForm" name="phoneNumber" placeholder="Phone Number*"><br>
+                            <button type="submit" class="btnSubmit">Submit</button> 
+                    </form>   
+                </div>
+            </div>  
         </div>
         <div id="sec2" class="sec">
         
@@ -18,7 +35,7 @@
         
         </div>
         <div class="my-footer">
-            
+
         </div>
     </body>
 </html>
